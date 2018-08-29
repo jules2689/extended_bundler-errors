7f6dc0ada5946d830dc8e97576d9fa348ac8d89a
jules2689 <julian@jnadeau.ca>
2018-08-29 14:33:32 -0400

    Bump version

--- lib/extended_bundler/errors/version.rb
+++ lib/extended_bundler/errors/version.rb
@@ -1,6 +1,6 @@
 module ExtendedBundler
   module Errors
-    VERSION = "0.2.5"
-    HOMEPAGE = "http://github.com/shopify/extended_bundler-errors"
+    VERSION = "0.2.6"
+    HOMEPAGE = "http://github.com/jules2689/extended_bundler-errors"
   end
 end
